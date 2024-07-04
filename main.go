9c3ca8212879de98b955906b1b8a9ddc8acc08ea
Artem Romanovich <art.rom.one@gmail.com>
2024-07-04 20:15:01 +0300

    decomposing check credentials logic

--- main.go
+++ main.go
@@ -15,31 +15,39 @@ func main() {
 	users := GetAllUserCredentials()
 
 	for userPrefix, credentials := range users {
-		fmt.Printf("Credentials for %s: %+v\n", userPrefix, credentials)
+		fmt.Printf("Checking credentials for %s...", userPrefix)
+		apiKey := credentials.APIKey
+
+		CheckCredentials(apiKey)
 	}
+}
 
-	dummyUser := users["USER1"]
+func CheckCredentials(apiKey string) {
 
-	apiKey := dummyUser.APIKey
-
-	req, err := http.NewRequest(http.MethodGet,
-		"https://api.track.toggl.com/api/v9/me", nil)
+	method, url := http.MethodGet, "https://api.track.toggl.com/api/v9/me"
+	req, err := http.NewRequest(method, url, nil)
 	if err != nil {
-		print(err)
+		req = nil
 	}
+
 	req.Header.Set("Content-Type", "application/json; charset=utf-8")
 	req.SetBasicAuth(apiKey, "api_token")
 
-    client := &http.Client{}
+	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Println("Error sending request:", err)
+		resp = nil
+	}
+
+	if err != nil {
+		fmt.Printf(" Error sending request: %v\n", err)
+		return
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Unexpected status code: %d\n", resp.StatusCode)
+		fmt.Printf(" Unexpected status code: %d\n", resp.StatusCode)
+	} else {
+		fmt.Printf(" Credentials are valid.\n")
 	}
-
-	fmt.Println("Response Status:", resp.Status)
 }
