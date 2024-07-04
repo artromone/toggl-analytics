4237ead199c16a9f4caa6dd6ebc19f1f3b9f1899
Artem Romanovich <art.rom.one@gmail.com>
2024-07-04 19:08:54 +0300

    loading data from .env

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+type UserCredentials struct {
+	APIKey      string
+	WorkspaceID string
+	FileName    string
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	user1 := UserCredentials{
+		APIKey:      os.Getenv("U1_API_KEY"),
+		WorkspaceID: os.Getenv("U1_WORKSPACE_ID"),
+		FileName:    os.Getenv("U1_FILE_NAME"),
+	}
+
+	fmt.Printf("User 1: %+v\n", user1)
 }
